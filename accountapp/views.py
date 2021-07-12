39b49b6b06b57bf3faa6c9b8120338f7f7bcb5b5
jin2313 <ljh8190@naver.com>
2021-07-12 15:10:00 +0900

    07/12 web 02 commit2

--- accountapp/views.py
+++ accountapp/views.py
@@ -12,9 +12,8 @@ def hello_world(request):
         new_hello_world.text = temp
         new_hello_world.save()
 
-        hello_world_list = HelloWorld.objects.all()
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     elif request.method == 'GET':
         hello_world_list = HelloWorld.objects.all()
         return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
