35a211064b1c7f79e36a86447a9444e520c57eeb
jin2313 <ljh8190@naver.com>
2021-07-12 15:10:50 +0900

    07/12 web 02 commit2

--- accountapp/views.py
+++ accountapp/views.py
@@ -1,7 +1,9 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from django.urls import reverse
+
 from accountapp.models import HelloWorld
 
 
