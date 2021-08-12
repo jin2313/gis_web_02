995ea4682d97825c3ab219912bc4b5dfa745e146
jin2313 <ljh8190@naver.com>
2021-08-12 15:42:08 +0900

    08/12 web 02 commit5

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    name = models.CharField(max_length=20, null=False)
+    description = models.CharField(max_length=200, null=False)  # 폼에 아무것도 적지 않아도 된다는 건 아님 -> 그 옵션은 blank
+    image = models.ImageField(upload_to='project/', null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
