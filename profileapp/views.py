e671e06c8a5ec0517cfbec02408b3b70cdc52e02
jin2313 <ljh8190@naver.com>
2021-08-18 09:12:57 +0900

    08/18 web 02 commit1

--- profileapp/views.py
+++ profileapp/views.py
@@ -10,6 +10,7 @@ from profileapp.decorators import profile_ownership_required
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
+
 @method_decorator(login_required, 'get')
 @method_decorator(login_required, 'post')
 class ProfileCreateView(CreateView):
